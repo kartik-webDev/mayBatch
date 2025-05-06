e616a579bf5c9c6a05ea6515521c859c6b48bf96
ABHINAV GOEL <ABHINAV GOEL@DESKTOP-0BO8DIV>
2025-05-06 21:36:34 +0530

    Again committed by abhi goel

--- newPackage/src/abhinavpackage/NewAbhinav.java
+++ newPackage/src/abhinavpackage/NewAbhinav.java
@@ -3,8 +3,10 @@ package abhinavpackage;
 public class NewAbhinav {
 
 	public static void main(String[] args) {
-		System.out.println("this is abhinav side testing");
-		System.out.println("this is abhinav and kartik");
-	}
-
+		int a=1;
+		int b=2;
+		int c=a+b;
+		System.out.println("Hello world");
+		System.out.println("The result is:" +c);
+    }
 }
