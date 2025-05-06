55be16a4edb97dbadda043952a06e879601fa74c
ABHINAV GOEL <ABHINAV GOEL@DESKTOP-0BO8DIV>
2025-05-06 21:36:54 +0530

    again committed by abhinav goel

--- newPackage/src/abhinavpackage/NewAbhinav.java
+++ newPackage/src/abhinavpackage/NewAbhinav.java
@@ -4,8 +4,8 @@ public class NewAbhinav {
 
 	public static void main(String[] args) {
 		int a=1;
-		int b=2;
-		int c=a+b;
+		float b=2.7f;
+		float c=a+b;
 		System.out.println("Hello world");
 		System.out.println("The result is:" +c);
     }
